4ff72e0f1b8ad22ad1a20da76b56b9be29b04911
Nick Maven <nicksonlagat@Nicks-MacBook-Pro.local>
2022-06-08 23:26:06 +0300

    refactored the yaml

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const core = require("@actions/core");
-const github = require("@actions/github")(async () => {
+const github = require("@actions/github");
+
+async () => {
   try {
     core.notice("calling my actions");
   } catch {
     core.setFailed(error.message);
   }
-});
+};
